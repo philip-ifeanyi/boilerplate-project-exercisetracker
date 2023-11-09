ece386c97a894da2d727e5b0e4e22c8dc1c18096
philip-ifeanyi <philipifeanyi2000@gmail.com>
2023-11-09 14:15:39 +0100

    updated starter

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+mongoose.connect(process.env.DATABASE_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+}).then(()=> console.log('Server Started')).catch((err)=> console.err(err))
 
 
 
